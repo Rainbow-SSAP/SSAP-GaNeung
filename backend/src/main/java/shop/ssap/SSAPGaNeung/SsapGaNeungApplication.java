d8b3f3a68a2614faa8d9c53f9f3f6ec279e1ef91
cipp00h <12seokeun@gmail.com>
2024-01-01 18:40:25 +0900

    build: [BE] 프로젝트 초기 개발 환경 셋팅
    - Spring Web, Spring Data JPA, Lombok, Validation, MariaDB Driver 설치
    - dockerfile 생성 및 docker-compose.yaml 파일 작성(mariadb 및 spring boot 설정)
    
    Closes #2

---
+++ backend/src/main/java/shop/ssap/SSAPGaNeung/SsapGaNeungApplication.java
@@ -0,0 +1,13 @@
+package shop.ssap.SSAPGaNeung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsapGaNeungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsapGaNeungApplication.class, args);
+	}
+
+}
